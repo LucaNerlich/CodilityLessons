35e3aaf437370278b84b9db387c61ac1cf803108
Luca Nerlich <business@lucanerlich.de>
2016-08-09 20:05:06 +0200

    added solution based on fabis idee

--- src/Lessons/Arrays/CyclicRotation.java
+++ src/Lessons/Arrays/CyclicRotation.java
@@ -1,24 +1,42 @@
 package Lessons.Arrays;
 
+import java.util.Random;
+
 /**
  * Created by lucan on 09.08.2016.
  */
 public class CyclicRotation {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4};
-        solution(a, 3);
-        System.out.println();
+        int[] a = new int[5000000];
+        int[] b = new int[5000000];
 
+        Random rand = new Random();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = rand.nextInt(10) + 1;
+            ;
+        }
+
+        long startTime = System.currentTimeMillis();
+        sol_luca(a, 1);
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime);
+
+        startTime = System.currentTimeMillis();
+        sol_fabian(b, 1);
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println(totalTime);
     }
 
-    public static int[] solution(int[] a, int k){
+    public static int[] sol_luca(int[] a, int k) {
 
         int swapA = a[1];
         int swapB;
 
-        for(int j = 0; j < k; j++) {
-            int end = a[a.length-1];
+        for (int j = 0; j < k; j++) {
+            int end = a[a.length - 1];
             for (int i = 0; i < a.length; i++) {
                 if (i <= a.length - 2) {
                     if (i == 0) {
@@ -34,13 +52,21 @@ public class CyclicRotation {
                 }
             }
         }
-
         return a;
     }
 
-    public static void swap(final int[] arr, final int pos1, final int pos2){
-        final int temp = arr[pos1];
-        arr[pos1] = arr[pos2];
-        arr[pos2] = temp;
+    public static int[] sol_fabian(int[] a, int k) {
+        int arrayLastIndex = a.length - 1;
+        for (int j = 0; j < k; j++) {
+            int temp = a[arrayLastIndex];
+            for (int i = arrayLastIndex; i >= 0; i--) {
+                if (i == 0) {
+                    a[0] = temp;
+                } else {
+                    a[i] = a[i - 1];
+                }
+            }
+        }
+        return a;
     }
 }
